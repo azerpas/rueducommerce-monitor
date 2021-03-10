8d1660dc17cbd2b5e2b2905b5918f4e6cd62bf93
azerpas <xskylix@hotmail.fr>
2021-03-10 11:25:13 +0100

    Fetch stock

--- index.ts
+++ index.ts
@@ -9,4 +9,15 @@ const API_URL="https://www.rueducommerce.fr/listingDyn?boutique_id=18&langue_id=
 	}
 	const js = JSON.parse(r.body);
 	console.info(`Slug ${js.slug}`);
+	const availables = js.produits.filter((p: any) => p.Disponibilite !== 'epuise' && p.shop_name == "Rue du Commerce");
+	if(availables.length === 0){
+		console.info("No stock available");
+	}else{
+		console.info(`Product${availables.length > 1 ? "s" : ""} found!`);
+		for(const p of availables){
+			console.log(`${p.fournisseur_nom} - ${p.produit_nom_nom} - ${p.prix_ttc}`);
+			console.log(`https://www.rueducommerce.fr${p.lien}`);
+		}
+	}	
+	process.exit(0);
 })();
