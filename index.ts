bc034ead6135a0aac325e5a49bfe664c6dd72937
azerpas <xskylix@hotmail.fr>
2021-03-10 11:54:34 +0100

    Added proxy support

--- index.ts
+++ index.ts
@@ -1,12 +1,25 @@
 import got from "got"
 import * as dotenv from 'dotenv';
+import 'global-agent/bootstrap';
 
 dotenv.config();
 
 const API_URL=process.env.API_URL || "https://www.rueducommerce.fr/listingDyn?boutique_id=18&langue_id=1&categoriesId=15333&nbImage=2&caracteristiquesValeursId=552298&from=0";
 const PRICE_LIMIT=1650.00;
+const PROXY_HOST = process.env.PROXY_HOST;
+const PROXY_PORT = process.env.PROXY_PORT;
+const PROXY_USER = process.env.PROXY_USER;
+const PROXY_PASSWORD = process.env.PROXY_PASSWORD;
 
 (async () => {
+	if(PROXY_HOST && PROXY_PORT){
+		console.log(`Setting proxy at host: ${PROXY_HOST}`);
+		if(PROXY_USER && PROXY_PASSWORD){
+			global.GLOBAL_AGENT.HTTP_PROXY = `http://${PROXY_USER}:${PROXY_PASSWORD}@${PROXY_HOST}:${PROXY_PORT}`;
+		}else{
+			global.GLOBAL_AGENT.HTTP_PROXY = `http://${PROXY_HOST}:${PROXY_PORT}`;
+		}
+	}
 	const r = await got(API_URL);
 	if(r.statusCode !== 200){
 		console.error(`Error while fetching the API: ${r.body}`);
